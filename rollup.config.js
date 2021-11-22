a15cd5a07e5573480154dd71e7e0789c0c20b429
Justin Poehnelt <jpoehnelt@google.com>
2021-11-22 13:36:07 -0700

    fix: update rollup plugins for declarations

--- rollup.config.js
+++ rollup.config.js
@@ -28,7 +28,8 @@ export default [
   {
     input: "src/index.ts",
     plugins: [
-      typescript(),
+      typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
+,
       commonjs(),
       babel(babelOptions),
       terser(terserOptions),
@@ -48,7 +49,8 @@ export default [
   },
   {
     input: "src/index.ts",
-    plugins: [typescript(), commonjs(), babel(babelOptions)],
+    plugins: [typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
+, commonjs(), babel(babelOptions)],
     output: {
       file: "dist/index.dev.js",
       format: "iife",
@@ -57,7 +59,8 @@ export default [
   },
   {
     input: "src/index.ts",
-    plugins: [typescript()],
+    plugins: [typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
+],
     output: {
       file: "dist/index.esm.js",
       format: "esm",
